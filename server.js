c76556bdc34052f8573b9c666d4c9c9d949ebdac
Mohak-787 <mohakdevkota62@gmail.com>
2026-02-26 17:00:50 +0545

    feat: add socket.io integration for real-time updates and user location tracking

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ dotenv.config();
 const http = require("http");
 const app = require("./src/app");
 const connectDB = require("./src/db/index");
+const { initializeSocket } = require("./src/socket.js");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +12,8 @@ const server = http.createServer(app);
 
 connectDB()
   .then(() => {
+    initializeSocket(server);
+
     server.listen(PORT, () => {
       console.log(`Server running at http://localhost:${PORT}`);
     });
